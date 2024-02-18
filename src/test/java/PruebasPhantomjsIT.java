2d851182c946deb6b69467d6e6e05206cd9f3fd9
K4STR0 <74238421+K4STR0@users.noreply.github.com>
2024-02-18 04:22:05 +0100

    Añadir test de votar a otro

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -50,4 +50,34 @@ public class PruebasPhantomjsIT {
     driver.close();
     driver.quit();
   }
+
+  @Test
+  public void votarOtroTest() {
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setJavascriptEnabled(true);
+    caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/bin/phantomjs");
+    caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
+        new String[] { "--web-security=no", "--ignore-ssl-errors=yes" });
+    driver = new PhantomJSDriver(caps);
+    driver.navigate().to("http://localhost:8080/Baloncesto/");
+    driver.findElement(By.name("R1")).click(); // Selector de votar a otro
+    driver.findElement(By.name("txtOtros")).sendKeys("Ruben");
+    driver.findElement(By.name("B1")).click(); // Boton de enviar voto
+    driver.navigate().to("http://localhost:8080/Baloncesto/");
+    List<WebElement> votes = driver.findElements(By.tagName("li"));
+
+    for (int i = 0; i < votes.size(); i++) {
+      String liText = votes.get(i).getText();
+      String name = liText.split(": ")[0];
+      String value = liText.split(": ")[1];
+
+      if (name.equals("Ruben")) {
+        assertEquals("1", value, "El numero de votos no es correcto");
+      } else {
+        assertEquals("0", value, "El numero de votos no es correcto");
+      }
+    }
+    driver.close();
+    driver.quit();
+  }
 }
